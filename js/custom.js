eb755b0ba79d6ad46277ef2924e9eaf56c987620
rambabu <rambabu.roges96@gmail.com>
2020-09-25 12:09:11 +0530

    .

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,129 @@
-
-
 /* clients */
 $(function () {
-    $('#clients-list').owlCarousel({
-        items: 5,
-        smartSpeed: 700,
-        loop: true,
-        autoplay: true,
-        autoplayHoverPause: true,
-    });
+  $("#clients-list").owlCarousel({
+    items: 5,
+    smartSpeed: 700,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+  });
 });
 
 /*Scroll down*/
 $(function () {
-    $('#nav-menu a').click(function (event) {
-        event.preventDefault();
-        let section = $(this).attr('href');
-        $('html, body').animate({
-            scrollTop: $(section).offset().top - 64
-        }, 1300, "easeInOutExpo");
-    })
+  $("#nav-menu a").click(function (event) {
+    event.preventDefault();
+    let section = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(section).offset().top - 64,
+      },
+      1300,
+      "easeInOutExpo"
+    );
+  });
 });
 
+$(function () {
+  new WOW().init();
+});
+
+$(".navbar a").on("click", function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+    const hash = this.hash;
+    $("html,body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800
+    );
+  }
+});
+
+/*contact */
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+// Check email is valid
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  let isRequired = false;
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+      isRequired = true;
+    } else {
+      showSuccess(input);
+    }
+  });
+
+  return isRequired;
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+  }
+}
+
+// Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Event listeners
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (!checkRequired([username, email, password, password2])) {
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
+  }
+});
